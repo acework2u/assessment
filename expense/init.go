7338653fa5871ca92db2cdead6d34c6597ea1458
Anon D <acework2u@gmail.com>
2023-01-07 16:31:55 +0700

    update init db

--- expense/init.go
+++ expense/init.go
@@ -1,9 +1,13 @@
 package expense
 
 type ExpenseInfo struct {
-	ID     int      `json:"id"`
+	ID     string   `json:"id"`
 	TITLE  string   `json:"title"`
 	AMOUNT float32  `json:"amount"`
 	NOTE   string   `json:"note"`
 	TAGS   []string `json:tags`
+}
+
+type Expenses struct {
+	Expenses []ExpenseInfo `json:"Expenses"`
 }
--- expense/read.go
+++ expense/read.go
@@ -1,13 +1,49 @@
 package expense
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func GetExpense(c echo.Context) error {
-	id := c.Param("id")
 
-	return c.String(http.StatusOK, id)
+	url := "postgres://yyrrnwpr:Avk9HhlWGrrmGcvGAhtC0nYrAj0JMQA6@john.db.elephantsql.com/yyrrnwpr"
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		log.Fatal("Connect to database err", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	} else {
+		log.Println("Connect database...")
+	}
+
+	sqlStmt := "SELECT id,title,amount,note,tags FROM expense order by id"
+
+	rows, err := db.Query(sqlStmt)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer rows.Close()
+
+	result := Expenses{}
+	// for rows.Next() {
+	// 	expense := Expenses{}
+	// 	err2 := rows.Scan(&expense.id, &expense.title, &expense.amount, &expense.note, &expense.tags)
+
+	// 	if err2 != nil {
+	// 		return err2
+	// 	}
+
+	// 	result.Expenses = append(result.Expenses, expense)
+	// }
+	log.Println(result)
+	// return c.JSON(http.StatusCreated, result)
+	return c.String(http.StatusOK, "Read")
 }
